af9ad26f76078c0a69761056dad46c65f93b4dda
kphongph <kphongph@gmail.com>
2017-06-07 13:18:49 +0700

    pre

--- stream.js
+++ stream.js
@@ -10,7 +10,198 @@ var viewdb = sublevel(levelup('./myView',{'valueEncoding':'json'}));
 
 var studentdb = viewdb.sublevel('student');
 
+var baseUrl = 'https://maas.nuqlis.com:9000';
+
 module.exports.db = viewdb;
+
+var getQinfoStudentList = function(chunk,students) {
+  return new Promise(function(fulfill,reject) {
+    var _classStr = chunk._info.educationclassid.split(' ');
+    var _classInt = 0;
+    var strId = null;
+    if(_classStr[0] == 'ประถมศึกษาปีที่') _classInt=3;
+    if(_classStr[0] == 'มัธยมศึกษาปีที่') _classInt=9;
+    _classInt+=Number(_classStr[1]);
+    if(_classInt > 0) {
+      var _queryUrl = 'https://newtestnew.azurewebsites.net/ServiceControl'+
+        '/GetEduService.svc/getFindChild?year='+
+        chunk._info.year+'&hostid='+
+        chunk._info.hostid+'&educlass='+
+        _classInt+'&room='+
+        chunk._info.room+'&text=';
+      console.log(_queryUrl);
+      request.get({
+        url:_queryUrl,
+        json:true
+      },function(err,response,body) {
+        var jsonObj = JSON.parse(body);
+        fulfill(_.union(students,_.map(jsonObj.persondata,'cid')));
+      });
+    } else {
+        fulfill([]);
+    }
+  });
+}
+
+var getObecStudentList = function(chunk) {
+  return new Promise(function(fulfill,reject) {
+    request.post({
+      url:baseUrl+'/api/query/obec_students/host_class_room',
+      json:true,
+      headers:{
+        Authorization:'JWT '+config.token
+      },
+      body:{'match':[chunk._info.hostid,
+        chunk._info.educationclassid,
+        chunk._info.room],'limit':-1,'include_doc':true}
+      },function(err,response,body) {
+        var students = [];
+        if(body.length!=0) {
+          students = _.filter(body,function(item) {
+            return !(item.value.doc.year == chunk._info.year && 
+              item.value.doc.semester  == chunk._info.semester);
+          });
+          students = _.map(students,'value.doc.cid');
+        } 
+        console.log('obec_student',students.length);
+        fulfill(chunk,students);
+      });
+    });
+}
+
+var getStudentInfo = function(chunk) {
+  return new Promise(function(fulfill,reject) {
+    var key = bytewise.encode([chunk._info.year,
+       chunk._info.semester,
+       chunk._info.hostid,
+       chunk._info.educationclassid,
+       chunk._info.room],'hex');
+    studentdb.get(key,function(err,doc) {
+      if(err) {
+        getObecStudentList(chunk).then(getQinfoStudentList)
+        .then(function(students) {
+          console.log('students',students.length);
+          studentdb.put(key,{'students':students},function() {
+            chunk['_student']=students;
+            fulfill(chunk);
+          });
+        },function() {
+          console.log('eee');
+        });
+      } else {
+        console.log('found student',doc.students.length);
+        chunk['_student']=doc.students;
+        fulfill(chunk);
+      }
+    });
+  });  
+};
+
+
+var getHostInfo = function(chunk) {
+  return new Promise(function(fulfill,reject) {
+    request.post({
+      url:baseUrl+'/api/query/school_db/hostid',
+      headers:{
+        Authorization:'JWT '+config.token
+      },
+      json:true,
+      body:{'match':[chunk._info.hostid],'include_doc':true,'limit':1}
+    },function(err,response,_host) {
+      if(err) { 
+        reject(err); 
+      } else {
+        if(_host.length == 1) {
+          chunk['_hostinfo']=_host[0].value.doc;
+          fulfill(chunk);
+        } else {
+          reject("no hostinfo"); 
+        }
+      }
+    });
+  });
+};
+
+var getMorningInfo = function(chunk) {
+  return new Promise(function(fulfill,reject) {
+    request.get({
+      url:baseUrl+'/api/dbs/morning/'+chunk.value.id,
+      json:true,
+      headers:{
+        Authorization:'JWT '+config.token
+      }
+    },function(err,response,body) {
+      if(err) {
+        reject(err); 
+      } else {
+        if(body.hostid) {
+          chunk['_info'] = body;
+          if(body.educationclassid.length == 0) {
+            reject("no educationclassid"); 
+          } else {
+            fulfill(chunk);
+          } 
+        } else {
+          reject("no morninginfo"); 
+        }
+      }
+    });
+  });
+};
+
+var setDocument = function(chunk) {
+  return new Promise(function(fulfill,reject) {
+    var _by_room = bytewise.encode(['by_room',
+      chunk._info.year,
+      chunk._info.semester,
+      chunk._info.hostid,
+      chunk._info.educationclassid,
+      chunk._info.room],'hex');
+    viewdb.get(_by_room,function(err,doc) {
+      if(err) {
+        var value = {'total':chunk._student.length};
+        if(chunk._student.indexOf(chunk.value.cid)!=-1) {
+           value[chunk.value.desc] = 1;
+        }
+        var doc = {};
+        doc['date_'+chunk._info.recdate] = value;
+        viewdb.put(_by_room,doc,function() {
+          fulfill(chunk);
+        });
+      } else {
+        // update
+        fulfill(chunk);
+        /*
+        var recdates = null;
+        var _sexists = false;
+        for(var i=0;i<doc.recdates.length;i++) {
+          if(doc.recdates[i].date == chunk._info.recdate) {
+            recdates = doc.recdates[i];
+            break;
+          }
+        }
+        if(recdates != null) {
+          for(var i=0;i<recdates.info.length;i++) {
+            if(recdates.info[i].cid == chunk.value.cid) {
+              recdates.info[i].desc = chunk.value.desc;
+              _sexists = true;
+              break;
+            }
+          }
+        }
+
+        if(recdates == null) {
+          var tmp = {'date':chunk._info.recdate,'info':[]};
+          if(chunk._student.indexOf(chunk.value.cid)!=-1) {
+            tmp.info.push({'cid':chunk.value.cid,'desc':chunk.value.desc});
+          }
+          doc.recdates.push(tmp);
+        };
+        */
+      }    
+    });
+  });
+}
 
 module.exports.createStreamHandlers = function(config) {
   return function() {
@@ -22,155 +213,24 @@ module.exports.createStreamHandlers = function(config) {
 
     var getInfo = function() {
       return through2.obj(function(chunk,enc,cb) {
+        console.log('getInfo',chunk._rev);
         var self = this;
-        request.get({
-          url:'https://maas.nuqlis.com:9000/api/dbs/morning/'+chunk.value.id,
-          json:true,
-          headers:{
-            Authorization:'JWT '+config.token
-          }
-        },function(err,response,body) {
-          if(body.hostid) {
-            chunk['_info']=body;
-            self.push(chunk);
-          }
-          cb();
+        getMorningInfo(chunk)
+        .then(getHostInfo)
+        .then(getStudentInfo)
+        .then(setDocument)
+        .then(function(_chunk) {
+          console.log('success');
+          console.log(_chunk);
+          cb(null,_chunk);
+        })
+        .catch(function(err) {
+          console.log('error',err);
+          cb(null,chunk);
         });
       });
     };
 
-    var getStudentInfo = function() {
-      return through2.obj(function(chunk,enc,cb) {
-        var self = this;
-        var key = bytewise.encode([chunk._info.year,
-          chunk._info.semester,
-          chunk._info.hostid,
-          chunk._info.educationclassid,
-          chunk._info.room],'hex');
-        studentdb.get(key,function(err,doc) {
-          if(err) {
-            request.post({
-              url:'https://maas.nuqlis.com:9000/api/query/obec_students/host_class_room',
-              json:true,
-              headers:{
-                Authorization:'JWT '+config.token
-              },
-              body:{'match':[chunk._info.hostid,chunk._info.educationclassid,chunk._info.room],'limit':-1,'include_doc':true}
-            },function(err,response,body) {
-              var students = [];
-              if(body.length!=0) {
-                students = _.filter(body,function(item) {
-                   return !(item.value.doc.year == chunk._info.year && 
-                     item.value.doc.semester  == chunk._info.semester);
-                });
-                students = _.map(students,'value.doc.cid');
-              } 
-              studentdb.put(key,{'students':students},function() {
-                chunk['_student']=students;
-                self.push(chunk);
-                cb();
-              });
-            });
-          } else {
-            chunk['_student']=doc.students;
-            self.push(chunk);
-            cb();
-          }
-        });
-      });
-    }
-    
-    var getHostInfo = function() {
-      return through2.obj(function(chunk,enc,cb) {
-        var self = this;
-        if(chunk._info.hostid && chunk._student.length>0) {
-          request.post({
-            url:'https://maas.nuqlis.com:9000/api/query/school_db/hostid',
-              headers:{
-              Authorization:'JWT '+config.token
-            },
-            json:true,
-            body:{'match':[chunk._info.hostid],'include_doc':true,'limit':1}
-          },function(err,response,_host) {
-            if(_host.length == 1) {
-              chunk['_hostinfo']=_host[0].value.doc;
-              self.push(chunk);
-              cb();
-            }
-          });
-        } else {
-          cb();
-        }
-      });
-    };
-
-    var setDocument = function() {
-      return through2.obj(function(chunk,enc,cb) {
-        var self = this;
-        var _by_room = bytewise.encode(['by_room',
-          chunk._info.year,
-          chunk._info.semester,
-          chunk._info.hostid,
-          chunk._info.educationclassid,
-          chunk._info.room],'hex');
-        viewdb.get(_by_room,function(err,doc) {
-          if(err) {
-            var value = {'recdates':[],'students':chunk._student};
-            var tmp = {'date':chunk._info.recdate,'info':[]};
-            if(chunk._student.indexOf(chunk.value.cid)!=-1) {
-              tmp.info.push({'cid':chunk.value.cid,'desc':chunk.value.desc});
-            }
-            value.recdates.push(tmp);
-            viewdb.put(_by_room,value,function() {
-              self.push(chunk);
-              cb();
-            });
-          } else {
-            // update
-            var recdates = null;
-            var _sexists = false;
-            for(var i=0;i<doc.recdates.length;i++) {
-              if(doc.recdates[i].date == chunk._info.recdate) {
-                recdates = doc.recdates[i];
-                break;
-              }
-            }
-            if(recdates != null) {
-              for(var i=0;i<recdates.info.length;i++) {
-                if(recdates.info[i].cid == chunk.value.cid) {
-                   recdates.info[i].desc = chunk.value.desc;
-                   _sexists = true;
-                   break;
-                }
-              }
-            }
-
-            if(recdates == null) {
-              var tmp = {'date':chunk._info.recdate,'info':[]};
-              if(chunk._student.indexOf(chunk.value.cid)!=-1) {
-                tmp.info.push({'cid':chunk.value.cid,'desc':chunk.value.desc});
-              }
-              doc.recdates.push(tmp);
-            } else {
-              if(!_sexists) {
-                if(chunk._student.indexOf(chunk.value.cid)!=-1) {
-                  recdates.info.push({'cid':chunk.value.cid,'desc':chunk.value.desc});
-                }
-              }
-            }
-
-            console.log(JSON.stringify(doc,null,2));
-            viewdb.put(_by_room,doc,function() {
-              self.push(chunk);
-              cb();
-            });
-          }
-        });
-        
-      });
-    };
-    return [dump];
-
-    // return [getInfo,getStudentInfo,getHostInfo,setDocument];
+    return [getInfo];
   };
 };
