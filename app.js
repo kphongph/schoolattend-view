6d03c2dc3d71a0ddfd287416b71adc54e4669e78
kphongph <kphongph@gmail.com>
2017-06-10 07:55:36 +0700

    search by key

--- app.js
+++ app.js
@@ -16,18 +16,38 @@ var attendDb = multilevel.client();
 var con = net.connect(3000);
 con.pipe(attendDb.createRpcStream()).pipe(con); 
 
+var encode = function(key) {
+  return Buffer.prototype.toString.call(bytewise.encode(key), 'hex');
+};
+
+var decode = function(key) {
+  return bytewise.decode(new Buffer(key, 'hex'));
+};
+  
 app.get('/by_room/:year/:semester/:host',function(req,res) {
-  console.log(req.params);
-  var start = ['by_room',Number(req.params.year),Number(req.params.semester),req.params.host];
+  var start = ['by_room',
+    Number(req.params.year),
+    Number(req.params.semester),
+    req.params.host];
   var end = start.slice();
   end.push(undefined);
-  
   attendDb.createReadStream({
- //   start:bytewise.encode(start,'hex'),
- //   end:bytewise.encode(end,'hex')
+    start:encode(start),
+    end:encode(end)
   })
   .pipe(through2.obj(function(chunk,enc,cb) {
-    chunk.key = bytewise.decode(new Buffer(chunk.key, 'hex'));
+    chunk.key = decode(chunk.key);
+    var val = {'gt15p':0};
+    var total = chunk.value['recdate'].length;
+    for(var key in chunk.value) {
+      if(key != 'recdate') {
+        if((chunk.value[key].length/total) > 0.15) {
+          val['gt15p']++;
+        }
+      }
+    }
+    val['raw']=chunk.value;
+    chunk.value = val;
     cb(null,chunk);
   }))
   .pipe(JSONStream.stringify())
